df1c88154bc384ad9f46c5fc186c7a79cf422c81
Keenan Gee <k33nang33@hotmail.co.uk>
2023-03-28 10:40:06 +0100

    finish :article_id end point and tests

--- __tests__/app.tests.js
+++ __tests__/app.tests.js
@@ -8,38 +8,16 @@ beforeEach(() => seed(testData));
 afterAll(() => db.end());
 
 describe("nc news app", () => {
-  describe("GET /api", () => {
-    test("status: 200, responds with a JSON describing all available endpoints", () => {
-      return request(app)
-        .get("/api")
-        .expect(200)
-        .then(({ body }) => {
-          expect(body).toEqual({
-            msg: "Server is up and running",
-          });
-        });
-    });
-    test("status: 404, responds with error message when a bad endpoint is used (typo in url)", () => {
-      return request(app)
-        .get("/api/bad")
-        .expect(404)
-        .then(({ body }) => {
-          expect(body).toEqual({
-            msg: "Route not found",
-          });
-        });
-    });
-  });
-
   describe("GET /api/topics", () => {
-    test('status: 200, responds with an object containing a key of "topics" and a value of an object', () => {
+    test('status: 200, responds with an object containing a key of "topics" and a value of an array of objects', () => {
       return request(app)
         .get("/api/topics")
         .expect(200)
         .then(({ body }) => {
           expect(Object.keys(body)[0]).toBe("topics");
 
-          expect(body.topics).toBeInstanceOf(Object);
+          expect(body.topics).toBeInstanceOf(Array);
+          expect(body.topics[0]).toBeInstanceOf(Object);
         });
     });
     test('status: 200, the value of the key "topics" should be an object containing an array of topic objects', () => {
@@ -63,6 +41,73 @@ describe("nc news app", () => {
         .then(({ body }) => {
           expect(body).toEqual({
             msg: "Route not found",
+          });
+        });
+    });
+  });
+  describe("GET /api/articles/:article_id", () => {
+    test('status: 200, responds with an object containing a key of "article" and a value of an object', () => {
+      return request(app)
+        .get("/api/articles/1")
+        .expect(200)
+        .then(({ body }) => {
+          expect(Object.keys(body)[0]).toBe("article");
+          expect(body.article).toBeInstanceOf(Object);
+        });
+    });
+    test("status: 200, responds with the correct article details matched with the article that has the id of 1", () => {
+      return request(app)
+        .get("/api/articles/1")
+        .expect(200)
+        .then(({ body }) => {
+          expect(body.article).toMatchObject({
+            article_id: 1,
+            author: "butter_bridge",
+            title: "Living in the shadow of a great man",
+            body: "I find this existence challenging",
+            topic: "mitch",
+            created_at: "2020-07-09T20:11:00.000Z",
+            votes: 100,
+            article_img_url:
+              "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+          });
+        });
+    });
+    test("status: 200, responds with the correct article details matched with the article that has the id of 7", () => {
+      return request(app)
+        .get("/api/articles/7")
+        .expect(200)
+        .then(({ body }) => {
+          expect(body.article).toMatchObject({
+            article_id: 7,
+            author: "icellusedkars",
+            title: "Z",
+            body: "I was hungry.",
+            topic: "mitch",
+            created_at: "2020-01-07T14:08:00.000Z",
+            votes: 0,
+            article_img_url:
+              "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+          });
+        });
+    });
+    test("status: 404, responds with an error message if the article id is not found, but is a valid number", () => {
+      return request(app)
+        .get("/api/articles/999")
+        .expect(404)
+        .then(({ body }) => {
+          expect(body).toEqual({
+            msg: "Article not found",
+          });
+        });
+    });
+    test("status: 400, responds with an error message if the article id is not a number", () => {
+      return request(app)
+        .get("/api/articles/not-a-number")
+        .expect(400)
+        .then(({ body }) => {
+          expect(body).toEqual({
+            msg: "Oops... Bad Request",
           });
         });
     });
--- controllers/errorHandle.controller.js
+++ controllers/errorHandle.controller.js
@@ -1,8 +1,23 @@
-exports.RouteNotFoundError = (err, req, res, next) => {
-  const { status = 404, msg } = err;
-  res.status(status).send({ msg });
+exports.RouteNotFoundError = (req, res, next) => {
+  res.status(404).send({ msg: "Route not found" });
+};
+
+exports.psql400Error = (err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Oops... Bad Request" });
+  } else {
+    next(err);
+  }
+};
+
+exports.customErrors = (err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
 };
 
 exports.serverError500 = (err, req, res, next) => {
-  res.status(500).send({ msg: "Internal Server Error" });
+  res.status(500).send({ msg: "Oh no... Internal Server Error!" });
 };
