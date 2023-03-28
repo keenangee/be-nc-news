250c27cb347cba4c1cd46f3a214d9a20b3673a5a
Keenan Gee <k33nang33@hotmail.co.uk>
2023-03-28 16:38:21 +0100

    refactor comments controller

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -8,9 +8,6 @@ exports.getCommentsByArticleId = (req, res, next) => {
     fetchArticleById(article_id),
   ])
     .then(([comments, article]) => {
-      if (!article) {
-        return Promise.reject({ status: 404, msg: "Article not found" });
-      }
       res.status(200).send({ comments });
     })
     .catch((err) => next(err));
