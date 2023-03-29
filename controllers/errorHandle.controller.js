10b50329788b78cb78d3106707aa592ec5bff735
Keenan Gee <k33nang33@hotmail.co.uk>
2023-03-29 10:57:34 +0100

    finish post comments end point with tests

--- controllers/errorHandle.controller.js
+++ controllers/errorHandle.controller.js
@@ -2,17 +2,28 @@ exports.RouteNotFoundError = (req, res, next) => {
   res.status(404).send({ msg: "Route not found" });
 };
 
-exports.psql400Error = (err, req, res, next) => {
-  if (err.code === "22P02") {
-    res.status(400).send({ msg: "Oops... Bad Request" });
+exports.customErrors = (err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
   } else {
     next(err);
   }
 };
 
-exports.customErrors = (err, req, res, next) => {
-  if (err.status && err.msg) {
-    res.status(err.status).send({ msg: err.msg });
+exports.psql400Error = (err, req, res, next) => {
+  const psqlCodes = ["22P02", "23502"];
+  if (psqlCodes.includes(err.code)) {
+    res.status(400).send({ msg: "Bad Request" });
+  } else {
+    next(err);
+  }
+};
+
+exports.psql404Error = (err, req, res, next) => {
+  const psqlCodesNotFound = ["23503"];
+  const psqlDetailWord = err.detail.match(/\(\w+\)/g)[0].slice(1, -1) || null;
+  if (psqlCodesNotFound.includes(err.code) && psqlDetailWord) {
+    res.status(404).send({ msg: `${psqlDetailWord} not found` });
   } else {
     next(err);
   }
