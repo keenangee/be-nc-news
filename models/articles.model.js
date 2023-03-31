f3f79f5431268c3964d5a729ad43a2589d35e874
Keenan Gee <k33nang33@hotmail.co.uk>
2023-03-31 09:09:36 +0100

    updated get article by id to also now show comment count with tests

--- models/articles.model.js
+++ models/articles.model.js
@@ -41,9 +41,12 @@ exports.fetchArticleById = (article_id) => {
   return db
     .query(
       `
-    SELECT *
+    SELECT articles.*, CAST(count(comments.comment_id)AS INTEGER) AS comment_count
     FROM articles
+    LEFT JOIN comments
+    ON articles.article_id = comments.article_id
     WHERE articles.article_id = $1
+    GROUP BY articles.article_id
         `,
       [article_id]
     )
