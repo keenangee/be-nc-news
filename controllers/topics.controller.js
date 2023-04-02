91b8960b497b8e256e9f2082d26cca4a8e9488b0
Keenan Gee <k33nang33@hotmail.co.uk>
2023-04-02 02:51:00 +0100

    finish post topics end point with tests and errors handled

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,9 +1,21 @@
-const { fetchTopics } = require("../models/topics.model");
+const { fetchTopics, insertTopic } = require("../models/topics.model");
 
 exports.getTopics = (req, res, next) => {
   fetchTopics()
     .then((topics) => {
       res.status(200).send({ topics });
     })
-    .catch(next);
+    .catch((err) => next(err));
+};
+
+exports.postTopics = (req, res, next) => {
+  const { slug, description } = req.body;
+  if (!slug || !description) {
+    next({ status: 400, msg: "Bad Request" });
+  }
+  insertTopic(slug, description)
+    .then((topic) => {
+      res.status(201).send({ topic });
+    })
+    .catch((err) => next(err));
 };
